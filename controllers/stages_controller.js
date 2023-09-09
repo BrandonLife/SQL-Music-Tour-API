09085a139658ba99565f9ba1bda3abbd55719b95
Brandon Life <blife1119@gmail.com>
2023-09-09 14:59:55 -0400

    finished making stage and event controllers

---
+++ controllers/stages_controller.js
@@ -0,0 +1,82 @@
+const stages = require('express').Router()
+const db = require('../models')
+
+const {Stage} = db
+
+const {Op} = require('sequelize')
+
+   
+// FIND ALL events
+stages.get('/', async (req, res) => {
+  try {
+      const foundStages = await Stage.findAll()
+      res.status(200).json(foundStages)
+  } catch (error) {
+      res.status(500).json(error)
+  }
+})
+
+
+
+
+//Find a specific event
+stages.get('/:id', async(req, res)=>{
+  try{
+    const foundStage = await Stage.findOne({
+      where: {stage_id: req.params.id}
+    })
+    res.status(200).json(foundStage)
+  }
+  catch(error){
+    res.status(500).json(error)
+  }
+  
+})
+
+//Create a event
+stages.post('/', async(req, res)=>{
+  try{
+    const newStage = await Stage.create(req.body)
+    res.status(200).json({
+      message: 'Successfully inserted a new Stage',
+      data: newStage
+    })
+  }
+  catch(error){
+    res.status(500).json(error)
+  }
+})
+
+//Update a event
+stages.put('/:id', async(req,res)=>{
+  try{
+    const updatedStage = await Stage.update(req.body, {
+      where: {stage_id: req.params.id}
+    })
+    res.status(200).json({message: `Successfully updated ${updatedStage} stage(s)`})
+  }
+  catch(error){
+    res.status(500).json(error)
+  }
+})
+
+
+// DELETE A event
+stages.delete('/:id', async (req, res) => {
+  try {
+      const deletedStage = await Stage.destroy({
+          where: {
+              stage_id: req.params.id
+          }
+      })
+      res.status(200).json({
+          message: `Successfully deleted ${deletedStage} stage(s)`
+      })
+  } catch(err) {
+      res.status(500).json(err)
+  }
+})
+
+//Export
+
+module.exports= stages
--- server.js
+++ server.js
@@ -15,9 +15,13 @@ app.get('/', (req, res) => {
     })
 })
 
-//Controller
+//Controllers
 const bandsController = require('./controllers/bands_controller')
 app.use('/bands', bandsController)
+const eventsController = require('./controllers/events_controller')
+app.use('/events', eventsController)
+const stagesController = require('./controllers/stages_controller')
+app.use('/stages', stagesController)
 
 // LISTEN
 app.listen(process.env.PORT, () => {
